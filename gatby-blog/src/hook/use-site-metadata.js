cd93219833545d950063b31761000a1149718109
OlushesiToheeb <olaolushesi98@gmail.com>
2020-12-05 16:55:18 +0100

    init commit

---
+++ gatby-blog/src/hook/use-site-metadata.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+export const useSiteMetadata = () => {
+  const { site } = useStaticQuery(
+    graphql`
+      query SiteMetaData {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+  )
+  return site.siteMetadata
+}
