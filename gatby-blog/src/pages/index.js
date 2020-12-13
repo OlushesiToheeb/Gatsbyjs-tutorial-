7ee235bedeb7e9fc216dc60a00d69e20ea33c549
OlushesiToheeb <olaolushesi98@gmail.com>
2020-12-13 17:56:06 +0100

    modify the index page

--- gatby-blog/src/pages/index.js
+++ gatby-blog/src/pages/index.js
@@ -1,9 +1,19 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitke = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
 
 export default ({ data }) => (
   <Layout>
@@ -13,9 +23,12 @@ export default ({ data }) => (
       <h4>{data.allMarkdownRemark.totalCount}</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
-          <span>
-            {node.frontmatter.title} - {node.frontmatter.date}
-          </span>
+          <BlogLink to={node.fields.slug}>
+            <BlogTitke>
+              {node.frontmatter.title} - {node.frontmatter.date}
+            </BlogTitke>
+          </BlogLink>
+
           <p>{node.excerpt}</p>
         </div>
       ))}
@@ -24,8 +37,8 @@ export default ({ data }) => (
 )
 
 export const query = graphql`
-  query MyQuery {
-    allMarkdownRemark {
+  query {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
       totalCount
       edges {
         node {
@@ -34,6 +47,9 @@ export const query = graphql`
           frontmatter {
             date
             title
+          }
+          fields {
+            slug
           }
         }
       }
